d049ef65c3b887c7c8ed88bc2d0bbb5257fe3f0f
Jomiel <enriquez.jliquigan@gmail.com>
2023-09-06 13:11:44 +0800

    Navigation Functionalities

--- AirWatch/Models/AppModel.cs
+++ AirWatch/Models/AppModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirWatch.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,52 @@ namespace AirWatch.Models
 {
     public class AppModel
     {
-        public TBL_ACCOUNTS Accounts { get; set; }
+        public Guid AccountID {
+            get 
+            {
+                if (HttpContext.Current.Session["AccountID"] != null)
+                {
+                    return new Guid(HttpContext.Current.Session["AccountID"].ToString());
+                }
+                return Guid.Empty;
+            } 
+        }
+        public TBL_ACCOUNTS Account {
+            get
+            {
+                if(HttpContext.Current.Session["currentUserAccount"] != null)
+                {
+                    return LoginRepository.GetAccountByAccountID(AccountID);
+                    //return HttpContext.Current.Session["currentUserAccount"] as TBL_ACCOUNTS;
+                }
+                return null;
+            } 
+        }
+        public ChangePass ChangePass { get; set; }
+        public string Message {
+            get
+            {
+                if (HttpContext.Current.Session["Message"] != null)
+                {
+                    return HttpContext.Current.Session["Message"] as string;
+                }
+                return null;
+            }
+        }
+        public string MessageType {
+            get
+            {
+                if (HttpContext.Current.Session["MessageType"] != null)
+                {
+                    return HttpContext.Current.Session["MessageType"] as string;
+                }
+                return null;
+            }
+        }
+    }
+    public class ChangePass
+    {
+        public string firstPass { get; set; }
+        public string secondPass { get; set; }
     }
 }
